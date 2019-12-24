651802b47e7b64268ff81c9e52e79a25418394fe
Ethan Tola <ethanltola@gmail.com>
2019-12-23 23:50:58 -0500

    Added cat combos in english.
    Signed-off-by: Ethan Tola <ethanltola@gmail.com>

--- src/python/unit_parser.py
+++ src/python/unit_parser.py
@@ -12,6 +12,7 @@ SCIPA_DB_CAT_UNIT = lambda unit_id: SCIPA_DB_BASE + "unit/%03d.html" % unit_id
 
 ENG_WIKI_BASE = "https://battle-cats.fandom.com"
 ENG_WIKI_CAT_NAMES = ENG_WIKI_BASE + "/wiki/Cat_Release_Order"
+ENG_WIKI_COMBOS = ENG_WIKI_BASE + "/wiki/Cat_Combo"
 ENG_WIKI_ENEMY_NAMES = ""
 
 MASSAGE = lambda data: data.text if data.font is None else data.font.text
@@ -19,7 +20,8 @@ MASSAGE = lambda data: data.text if data.font is None else data.font.text
 NUMBER_FORM_REGEX = re.compile(r"No.(?P<number>(\d+))-(?P<form>(\d))")
 
 UNIT_COMBO_SPLIT = "<hr class=\"line\"/>"
-UNIT_COMBO_NAME_REGEX = re.compile(r"<font class=\"H\">(\D+)</font>")
+UNIT_COMBO_JP_NAME_REGEX = re.compile(r"<font class=\"H\">(\D+)</font>")
+UNIT_COMBO_EN_NAME_REGEX = re.compile(r"<a[^>]+>(?P<name>[^<]+)</a>")
 UNIT_COMBO_UNIT_REGEX = re.compile(r"<a href=\"(\d+).html\">")
 
 UNIT_ABIL_ICON_REGEX = re.compile(r"<img class=\"icon_s\" src=\"https://battlecats-db.imgs-server.com/icon_s_(?P<ability>\d+).png\"/>")
@@ -45,28 +47,29 @@ def parse_cat_unit(unit_num):
 	print("Processing unit %03d" % unit_num)
 	unit = UnitDB()
 	scipa_soup = _url_to_soup(SCIPA_DB_CAT_UNIT(unit_num), check=True)
-	eng_soup = _url_to_soup(ENG_WIKI_CAT_NAMES, check=True)
+	eng_name_soup = _url_to_soup(ENG_WIKI_CAT_NAMES, check=True)
+	eng_combo_soup = _url_to_soup(ENG_WIKI_COMBOS, check=True)
 
 	if scipa_soup is None:
 		print("Skipping unit %03d" % unit_num)
 		return
 
 	scipa_table = scipa_soup.find('table')
-	eng_table = eng_soup.find('table')
+	eng_data = [ eng_name_soup.find('table'), eng_combo_soup ]
 
-	if scipa_table is None or eng_table is None:
+	if scipa_table is None or eng_data is None:
 		print("Skipping unit %03d" % unit_num)
 
 	for n, scipa_data in enumerate(scipa_table.children):
 		raw_data = list(scipa_data.children)
-		_parse_cat_unit_raw(raw_data, eng_table, unit, n)
+		_parse_cat_unit_raw(raw_data, eng_data, unit, n)
 
 	if len(unit) == 0:
 		print("Skipping unit %03d" % unit_num)
 		return
 
 	try:
-		_parse_cat_unit_en_description(eng_table, unit)
+		_parse_cat_unit_en_description(eng_data, unit)
 	except IndexError as ie:
 		print("{} not avaliable for unit #{}. Skipping".format("Description", unit.unitNumber))
 	except Exception as e:
@@ -79,12 +82,12 @@ def parse_cat_unit(unit_num):
 
 	return unit
 
-def _parse_cat_unit_raw(raw_data, eng_table, unit, row):
+def _parse_cat_unit_raw(raw_data, eng_data, unit, row):
 	"""Helper method to begin filling the UnitDB.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		row {integer} -- The current row to parse.
 	"""
@@ -101,11 +104,11 @@ def _parse_cat_unit_raw(raw_data, eng_table, unit, row):
 			saved_n = row
 
 		if got_a_form is 1:
-			_parse_form(raw_data, eng_table, unit, row, "Normal")
+			_parse_form(raw_data, eng_data, unit, row, "Normal")
 		elif got_a_form is 2:
-			_parse_form(raw_data, eng_table, unit, row, "Evolved")
+			_parse_form(raw_data, eng_data, unit, row, "Evolved")
 		elif got_a_form is 3:
-			_parse_form(raw_data, eng_table, unit, row, "True")
+			_parse_form(raw_data, eng_data, unit, row, "True")
 
 	except IndexError as ie:
 		print("{} not avaliable for unit #{}-{}. Skipping".format(_get_error_from_row(row), unit.unitNumber, got_a_form))
@@ -115,62 +118,62 @@ def _parse_cat_unit_raw(raw_data, eng_table, unit, row):
 		traceback.print_tb(e.__traceback__)
 		print(e)
 
-def _parse_form(raw_data, eng_table, unit, row, form):
+def _parse_form(raw_data, eng_data, unit, row, form):
 	"""Fills the cat UnitDB with data for a particular form.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		row {integer} -- The current row to parse.
 		form {string} -- A key detailing which form to fill.
 	"""
 	global saved_n
 	if row == saved_n:
-		_parse_cat_unit_name_and_version(raw_data, eng_table, unit, form)
+		_parse_cat_unit_name_and_version(raw_data, eng_data, unit, form)
 	elif row == saved_n + 2:
-		_parse_cat_unit_stats_1(raw_data, eng_table, unit, form)
+		_parse_cat_unit_stats_1(raw_data, eng_data, unit, form)
 	elif row == saved_n + 3:
-		_parse_cat_unit_stats_2(raw_data, eng_table, unit, form)
+		_parse_cat_unit_stats_2(raw_data, eng_data, unit, form)
 	elif row == saved_n + 4:
-		_parse_cat_unit_stats_3(raw_data, eng_table, unit, form)
+		_parse_cat_unit_stats_3(raw_data, eng_data, unit, form)
 	elif row == saved_n + 5:
-		_parse_cat_unit_stats_4(raw_data, eng_table, unit, form)
+		_parse_cat_unit_stats_4(raw_data, eng_data, unit, form)
 	elif row == saved_n + 6:
-		_parse_cat_unit_abilities(raw_data, eng_table, unit, form)
+		_parse_cat_unit_abilities(raw_data, eng_data, unit, form)
 	elif row == saved_n + 7:
-		_parse_cat_unit_jp_description(raw_data, eng_table, unit, form)
+		_parse_cat_unit_jp_description(raw_data, eng_data, unit, form)
 	elif row == saved_n + 8:
-		_parse_cat_unit_obtain_condition(raw_data, eng_table, unit, form)
+		_parse_cat_unit_obtain_condition(raw_data, eng_data, unit, form)
 	elif row == saved_n + 9:
-		_parse_cat_unit_combos(raw_data, eng_table, unit, form)
+		_parse_cat_unit_combos(raw_data, eng_data, unit, form)
 
-def _parse_cat_unit_name_and_version(raw_data, eng_table, unit, form):
+def _parse_cat_unit_name_and_version(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB name and version.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
 	unit[form].jpName = raw_data[1].text
 	if form == "Normal":
-		unit[form].enName = list(list(eng_table.children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].text.strip()
+		unit[form].enName = list(list(eng_data[0].children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].text.strip()
 	elif form == "Evolved":
-		unit[form].enName = list(list(eng_table.children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[4].text.split('/')[0].strip()
+		unit[form].enName = list(list(eng_data[0].children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[4].text.split('/')[0].strip()
 	elif form == "True":
-		unit[form].enName = list(list(eng_table.children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[4].text.split('/')[1].strip()
+		unit[form].enName = list(list(eng_data[0].children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[4].text.split('/')[1].strip()
 	else:
 		unit[form].enName = "No En Name Available"
 	unit[form].version = raw_data[2].text.strip(VERSION_STRIP)
 
-def _parse_cat_unit_stats_1(raw_data, eng_table, unit, form):
+def _parse_cat_unit_stats_1(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB health, knockback count, and attack rates.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
@@ -181,12 +184,12 @@ def _parse_cat_unit_stats_1(raw_data, eng_table, unit, form):
 	unit[form].stats['attackRateF'] = MASSAGE(raw_data[7])
 	unit[form].stats['attackRateS'] = MASSAGE(raw_data[8]).strip(ATTACK_RATE_STRIP)
 
-def _parse_cat_unit_stats_2(raw_data, eng_table, unit, form):
+def _parse_cat_unit_stats_2(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB attack power, movement speed, and attack animation time.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
@@ -195,12 +198,12 @@ def _parse_cat_unit_stats_2(raw_data, eng_table, unit, form):
 	unit[form].stats['attackAnimF'] = MASSAGE(raw_data[5])
 	unit[form].stats['attackAnimS'] = MASSAGE(raw_data[6]).strip(ATTACK_RATE_STRIP)
 
-def _parse_cat_unit_stats_3(raw_data, eng_table, unit, form):
+def _parse_cat_unit_stats_3(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB range and respawn times.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
@@ -208,12 +211,12 @@ def _parse_cat_unit_stats_3(raw_data, eng_table, unit, form):
 	unit[form].stats['respawnTimeF'] = MASSAGE(raw_data[7])
 	unit[form].stats['respawnTimeS'] = MASSAGE(raw_data[8]).strip(ATTACK_RATE_STRIP)
 
-def _parse_cat_unit_stats_4(raw_data, eng_table, unit, form):
+def _parse_cat_unit_stats_4(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB attack type and cost.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
@@ -221,12 +224,12 @@ def _parse_cat_unit_stats_4(raw_data, eng_table, unit, form):
 	unit[form].stats['attackType'] += "(Single Attack)" if MASSAGE(raw_data[3]) == SINGLE_ATTACK else "(Area Attack)"
 	unit[form].stats['cost'] = MASSAGE(raw_data[5])
 
-def _parse_cat_unit_abilities(raw_data, eng_table, unit, form):
+def _parse_cat_unit_abilities(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB abilities.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 
@@ -249,78 +252,94 @@ def _parse_cat_unit_abilities(raw_data, eng_table, unit, form):
 			unit[form].abilities["target"].append("All (w/o Metal/White)")
 		else:
 			for target in targ_arr:
-				if target:
+				if target and target.capitalize() not in unit[form].abilities["target"]:
 					unit[form].abilities["target"].append(target.capitalize())
 	except KeyError:
 		print("Unit #{}-{}: Ability {} not in DB".format(unit.unitNumber, form, icon_num))
 		raise IndexError
 
-def _parse_cat_unit_jp_description(raw_data, eng_table, unit, form):
+def _parse_cat_unit_jp_description(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB japanese descriptions.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
 	unit[form].jpDescription = MASSAGE(raw_data[1])
 
-def _parse_cat_unit_en_description(eng_table, unit):
+def _parse_cat_unit_en_description(eng_data, unit):
 	"""Parses a cat UnitDB english descriptions.
 
+	This is called outside the main loop as the English Wiki doesn't separate
+	cats based on their form.
+
 	Arguments:
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 
 	Raises:
 		IndexError: Raised when there is no english version of the japanese cat.
 	"""
-	unit_soup = _url_to_soup(ENG_WIKI_BASE + str(list(list(eng_table.children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].a["href"]))
+	unit_soup = _url_to_soup(ENG_WIKI_BASE + str(list(list(eng_data[0].children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].a["href"]))
 	if unit_soup is None:
-		print("Can't find unit at " + ENG_WIKI_BASE + str(list(list(eng_table.children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].a["href"]))
+		print("Can't find unit at " + ENG_WIKI_BASE + str(list(list(eng_data[0].children)[5 + ((int(unit.unitNumber) - 1) * 2)].children)[3].a["href"]))
 		raise IndexError
-	description = 0
 	for i, tag in enumerate(unit_soup.find_all('table')[1].children):
+		# There are two different forms of tables on the english wiki.
 		if "Description" in str(tag):
-			description = i
-		if description:
-			unit["Normal"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[description + 2]).strip()
-			unit["Evolved"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[description + 8]).strip()
+			unit["Normal"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i + 2]).strip()
+			unit["Evolved"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i + 8]).strip()
 			if unit["True"]:
-				unit["True"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[description + 14]).strip()
+				unit["True"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i + 14]).strip()
+			break
+		if "description" in str(tag):
+			unit["Normal"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i].find_all('td')[1]).strip()
+			unit["Evolved"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i+2].find_all('td')[1]).strip()
+			if unit["True"]:
+				unit["True"].enDescription = MASSAGE(list(unit_soup.find_all('table')[1].children)[i+4].find_all('td')[1]).strip()
 			break
 
-def _parse_cat_unit_obtain_condition(raw_data, eng_table, unit, form):
+def _parse_cat_unit_obtain_condition(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB various obtain conditions.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
 	# TODO: Obtain condition
 	pass
 
-def _parse_cat_unit_combos(raw_data, eng_table, unit, form):
+def _parse_cat_unit_combos(raw_data, eng_data, unit, form):
 	"""Parses a cat UnitDB combo list.
 
 	Arguments:
 		raw_data {bs4.BeautifulSoup} -- A table containing the cat unit data from SPICA.
-		eng_table {bs4.BeautifulSoup} -- A table containing the cat unit data from BattleCatsWiki.
+		eng_data {bs4.BeautifulSoup} -- An array containing cat unit data from BattleCatsWiki.
 		unit {UnitDB} -- A reference to cat UnitDB object.
 		form {string} -- A key detailing which form to fill.
 	"""
 	for combo in str(raw_data[1]).split(UNIT_COMBO_SPLIT):
-		name = UNIT_COMBO_NAME_REGEX.search(combo)
-		if name == None:
+		jp_name = UNIT_COMBO_JP_NAME_REGEX.search(combo)
+		if jp_name == None:
 			continue
-		name = name.group(1)
-		if "</font>" in name:
-			name = name.split("</font>")[0]
+		jp_name = jp_name.group(1)
+		if "</font>" in jp_name:
+			jp_name = jp_name.split("</font>")[0]
 		unit_list = UNIT_COMBO_UNIT_REGEX.findall(combo)
-		unit[form].combos[name] = unit_list
+
+		en_name = ""
+		eng_combo_list = str(eng_data[1]).splitlines()
+		for i, combo_line in enumerate(eng_combo_list):
+			if jp_name in combo_line:
+				en_name = UNIT_COMBO_EN_NAME_REGEX.search(eng_combo_list[i-1]).group('name')
+				en_name = en_name.replace('amp;', '')
+				break
+
+		unit[form].combos[jp_name + "/" + en_name] = unit_list
 
 def _get_base_stat(unit_number, health_lv):
 	"""Helper method to get base stat from SPICA's default database level.
